dcd4d36ec07b6b8aac67b10ec797fd28e0bca877
Listopia-Official <florhas@t-online.de>
2016-12-26 17:14:02 +0100

    Added UserBeanLocal

---
+++ lucas-ejb/ejbModule/florian_haas/lucas/business/UserBeanLocal.java
@@ -0,0 +1,65 @@
+package florian_haas.lucas.business;
+
+import java.util.List;
+
+import javax.ejb.Local;
+
+import florian_haas.lucas.database.EnumQueryComparator;
+import florian_haas.lucas.model.*;
+
+@Local
+public interface UserBeanLocal {
+
+	public User createPupil(String forename, String surname, Integer schoolGrade, String schoolClass, String... ranks);
+
+	public User createTeacher(String forename, String surname, String... ranks);
+
+	public User createGuest();
+
+	public List<User> findAll();
+
+	public User findById(Long userId);
+
+	public List<User> findByForename(String forename);
+
+	public List<User> findBySurname(String forename);
+
+	public List<User> findBySchoolGrade(Integer schoolGrade);
+
+	public List<User> findBySchoolClass(String schoolClass);
+
+	public List<User> findByUserType(EnumUserType userType);
+
+	public List<User> findByRanks(String... ranks);
+
+	public List<User> findUsers(Long userId, String forename, String surname, Integer schoolGrade, String schoolClass,
+			EnumUserType userType, String[] ranks, Boolean useUserId, Boolean useForename, Boolean useSurname, Boolean useSchoolGrade,
+			Boolean useSchoolClass, Boolean useUserType, Boolean useRanks, EnumQueryComparator userIdComparator,
+			EnumQueryComparator forenameComparator, EnumQueryComparator surnameComparator, EnumQueryComparator schoolGradeComparator,
+			EnumQueryComparator schoolClassComparator, EnumQueryComparator ranksComparator);
+
+	public User setForename(Long userId, String forename);
+
+	public User setSurname(Long userId, String surname);
+
+	public User setSchoolGrade(Long userId, Integer schoolGrade);
+
+	public User setSchoolClass(Long userId, String schoolClass);
+
+	public User addRank(Long userId, String rank);
+
+	public User removeRank(Long userId, String rank);
+
+	public User addUserCard(Long userId);
+
+	public boolean blockUserCard(Long userId);
+
+	public boolean unblockUserCard(Long userId);
+
+	public User addVisa(Long userId);
+
+	public boolean activateVisa(Long visaId);
+
+	public boolean deactivateVisa(Long visaId);
+
+}
