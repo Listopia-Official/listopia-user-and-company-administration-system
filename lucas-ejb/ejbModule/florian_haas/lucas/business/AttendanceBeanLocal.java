1472479ea900425208b1a53f19e9ecdc1b3232ee
Listopia-Official <florhas@t-online.de>
2016-12-26 17:28:19 +0100

    Added AttendanceBeanLocal

---
+++ lucas-ejb/ejbModule/florian_haas/lucas/business/AttendanceBeanLocal.java
@@ -0,0 +1,37 @@
+package florian_haas.lucas.business;
+
+import java.util.List;
+
+import javax.ejb.Local;
+
+import florian_haas.lucas.database.EnumQueryComparator;
+import florian_haas.lucas.model.Attendancedata;
+
+@Local
+public interface AttendanceBeanLocal {
+
+	public boolean scan(Long userCardId);
+
+	public void evaluateAll();
+
+	public List<Attendancedata> findAll();
+
+	public Attendancedata findById(Long id);
+
+	public Attendancedata findByUserId(Long userId);
+
+	public Attendancedata findByUserCardId(Long userCardId);
+
+	public List<Attendancedata> findByIsUserInState(Boolean isUserInState);
+
+	public List<Attendancedata> findByTimePresentDay(Long timePresentDay);
+
+	public List<Attendancedata> findByValidTimeMissing(Long validTimeMissing);
+
+	public List<Attendancedata> findAttendancedata(Long id, Long userId, Long userCardId, Boolean isUserInState, Long timePresentDay,
+			Long validTimeMissing, Boolean useId, Boolean useUserId, Boolean useUserCardId, Boolean useIsUserInState,
+			Boolean useTimePresentDay, Boolean useVaidTimeMissing, EnumQueryComparator idComparator,
+			EnumQueryComparator userIdComparator, EnumQueryComparator userCardIdComparator,
+			EnumQueryComparator timePresentDayComparator, EnumQueryComparator validTimeMissingComparator);
+
+}
