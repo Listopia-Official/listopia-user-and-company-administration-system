48160df303dae9379a335ace5b0633be2e13bf94
Listopia-Official <florhas@t-online.de>
2016-12-27 15:46:52 +0100

    Added BasicDAO interfaces

---
+++ lucas-ejb/ejbModule/florian_haas/lucas/database/DAO.java
@@ -0,0 +1,15 @@
+package florian_haas.lucas.database;
+
+import florian_haas.lucas.model.EntityBase;
+
+public interface DAO<E extends EntityBase> extends ReadOnlyDAO<E> {
+
+	public void persist(E entity);
+
+	public E refresh(E entity);
+
+	public E merge(E entity);
+
+	public void delete(E entity);
+
+}
---
+++ lucas-ejb/ejbModule/florian_haas/lucas/database/ReadOnlyDAO.java
@@ -0,0 +1,29 @@
+package florian_haas.lucas.database;
+
+import java.util.List;
+import java.util.function.BiFunction;
+
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.*;
+
+import florian_haas.lucas.model.EntityBase;
+
+public interface ReadOnlyDAO<E extends EntityBase> {
+
+	public EntityManager getEntityManager();
+
+	public Class<E> getEntityClass();
+
+	public List<E> findAll();
+
+	public E findById(Long id);
+
+	public Boolean exists(Long id);
+
+	public List<E> readOnlyJPQLQuery(String jpql, Object... params);
+
+	public List<E> readOnlyCriteriaQuery(BiFunction<Root<E>, CriteriaBuilder, Predicate[]> restrictions);
+
+	public List<E> readOnlyCriteriaQuery(BiFunction<Root<E>, CriteriaBuilder, Predicate[]> restrictions, Order[] orders);
+
+}
