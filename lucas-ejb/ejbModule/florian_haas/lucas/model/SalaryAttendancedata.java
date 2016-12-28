d28496e8750e1e53bc75c5ec8912b03cc387a05f
Listopia-Official <florhas@t-online.de>
2016-12-28 18:12:09 +0100

    Added SalaryAttendancedata

---
+++ lucas-ejb/ejbModule/florian_haas/lucas/model/SalaryAttendancedata.java
@@ -0,0 +1,54 @@
+package florian_haas.lucas.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+
+@Entity
+public class SalaryAttendancedata extends EntityBase {
+
+	private static final long serialVersionUID = 6663270725797916280L;
+
+	private SalaryData salaryData;
+	private LocalDate date;
+	private EnumWorkShift workShift;
+	private Boolean wasPresent = Boolean.FALSE;
+
+	SalaryAttendancedata() {}
+
+	public SalaryAttendancedata(SalaryData salaryData, LocalDate date, EnumWorkShift workShift, Boolean wasPresent) {
+		this.salaryData = salaryData;
+		this.date = date;
+		this.workShift = workShift;
+		this.wasPresent = wasPresent;
+	}
+
+	public SalaryData getSalaryData() {
+		return this.salaryData;
+	}
+
+	public LocalDate getDate() {
+		return this.date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public EnumWorkShift getWorkShift() {
+		return this.workShift;
+	}
+
+	public void setWorkShift(EnumWorkShift workShift) {
+		this.workShift = workShift;
+	}
+
+	public Boolean getWasPresent() {
+		return this.wasPresent;
+	}
+
+	public void setWasPresent(Boolean wasPresent) {
+		this.wasPresent = wasPresent;
+	}
+
+}
