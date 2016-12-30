6d483e91ffe008b801786ad7b96d8929ab1e544f
Listopia-Official <florhas@t-online.de>
2016-12-30 17:15:12 +0100

    Added UserCardDAOImpl

---
+++ lucas-ejb/ejbModule/florian_haas/lucas/database/impl/UserCardDAOImpl.java
@@ -0,0 +1,7 @@
+package florian_haas.lucas.database.impl;
+
+import florian_haas.lucas.database.*;
+import florian_haas.lucas.model.UserCard;
+
+@JPADAO
+public class UserCardDAOImpl extends ReadOnlyDAOImpl<UserCard> implements UserCardDAO {}
