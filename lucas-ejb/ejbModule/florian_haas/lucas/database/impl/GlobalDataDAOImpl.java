22be490218c2d9ea0aa485d2277391873a1fc6b9
Listopia-Official <florhas@t-online.de>
2016-12-30 17:00:34 +0100

    Added GlobalDataDAOImpl

---
+++ lucas-ejb/ejbModule/florian_haas/lucas/database/impl/GlobalDataDAOImpl.java
@@ -0,0 +1,7 @@
+package florian_haas.lucas.database.impl;
+
+import florian_haas.lucas.database.*;
+import florian_haas.lucas.model.GlobalData;
+
+@JPADAO
+public class GlobalDataDAOImpl extends ReadOnlyDAOImpl<GlobalData> implements GlobalDataDAO {}
