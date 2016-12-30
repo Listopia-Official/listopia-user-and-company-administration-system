2d00afa384388e594329dd7b638fe9c8bf18352e
Listopia-Official <florhas@t-online.de>
2016-12-30 17:14:15 +0100

    ItemDAO now extends DAO<Item>

--- lucas-ejb/ejbModule/florian_haas/lucas/database/ItemDAO.java
+++ lucas-ejb/ejbModule/florian_haas/lucas/database/ItemDAO.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 import florian_haas.lucas.model.Item;
 
-public interface ItemDAO extends ReadOnlyDAO<Item> {
-	public List<Item> findItems(Long itemId, String name, String description, Integer itemsAvaible, BigDecimal pricePerItem,
-			Boolean useId, Boolean useName, Boolean useDescription, Boolean useItemsAvaible, Boolean usePricePerItem,
-			EnumQueryComparator idComparator, EnumQueryComparator nameComparator, EnumQueryComparator descriptionComparator,
-			EnumQueryComparator itemsAvaibleComparator, EnumQueryComparator pricePerItemComparator);
+public interface ItemDAO extends DAO<Item> {
+	public List<Item> findItems(Long itemId, String name, String description, Integer itemsAvaible, BigDecimal pricePerItem, Boolean useId,
+			Boolean useName, Boolean useDescription, Boolean useItemsAvaible, Boolean usePricePerItem, EnumQueryComparator idComparator,
+			EnumQueryComparator nameComparator, EnumQueryComparator descriptionComparator, EnumQueryComparator itemsAvaibleComparator,
+			EnumQueryComparator pricePerItemComparator);
 }
