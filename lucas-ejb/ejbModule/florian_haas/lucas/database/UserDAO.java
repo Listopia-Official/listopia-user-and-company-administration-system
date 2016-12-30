bc9baad0d6d684433892b7172b0c26faa165bf31
Listopia-Official <florhas@t-online.de>
2016-12-30 16:15:47 +0100

    Added UserDAO

---
+++ lucas-ejb/ejbModule/florian_haas/lucas/database/UserDAO.java
@@ -0,0 +1,15 @@
+package florian_haas.lucas.database;
+
+import java.util.List;
+
+import florian_haas.lucas.model.*;
+
+public interface UserDAO extends DAO<User> {
+
+	public List<User> findUsers(Long userId, String forename, String surname, Integer schoolGrade, String schoolClass,
+			EnumUserType userType, String[] ranks, Boolean useUserId, Boolean useForename, Boolean useSurname, Boolean useSchoolGrade,
+			Boolean useSchoolClass, Boolean useUserType, Boolean useRanks, EnumQueryComparator userIdComparator,
+			EnumQueryComparator forenameComparator, EnumQueryComparator surnameComparator, EnumQueryComparator schoolGradeComparator,
+			EnumQueryComparator schoolClassComparator, EnumQueryComparator ranksComparator);
+
+}
