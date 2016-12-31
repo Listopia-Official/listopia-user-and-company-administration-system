05119f6aa044836a6e0ca15a367a7c6426b88f80
Listopia-Official <florhas@t-online.de>
2016-12-31 16:49:22 +0100

    Added TransactionLog class-level validation constraints

---
+++ lucas-ejb/ejbModule/florian_haas/lucas/model/validation/ValidTransactionLog.java
@@ -0,0 +1,21 @@
+package florian_haas.lucas.model.validation;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.*;
+
+import javax.validation.*;
+
+@Target({
+		TYPE, ANNOTATION_TYPE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = ValidTransactionLogValidator.class)
+@Documented
+public @interface ValidTransactionLog {
+	String message() default "Invalid transaction log";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+}
---
+++ lucas-ejb/ejbModule/florian_haas/lucas/model/validation/ValidTransactionLogValidator.java
@@ -0,0 +1,21 @@
+package florian_haas.lucas.model.validation;
+
+import javax.validation.*;
+
+import florian_haas.lucas.model.*;
+import florian_haas.lucas.util.Utils;
+
+public class ValidTransactionLogValidator implements ConstraintValidator<ValidTransactionLog, TransactionLog> {
+
+	@Override
+	public void initialize(ValidTransactionLog constraintAnnotation) {}
+
+	@Override
+	public boolean isValid(TransactionLog value, ConstraintValidatorContext context) {
+		if (value == null) return true;
+		return (value.getAction() == EnumAccountAction.DEBIT ? Utils.isGreatherEqualZero(value.getPreviousBankBalance().subtract(value.getAmount()))
+				: true) && (value.getActionType() == EnumAccountActionType.TRANSACTION ? value.getRelatedAccount() != null : true)
+				&& (value.getActionType() == EnumAccountActionType.BANK ? value.getRelatedAccount() == null : true);
+	}
+
+}
