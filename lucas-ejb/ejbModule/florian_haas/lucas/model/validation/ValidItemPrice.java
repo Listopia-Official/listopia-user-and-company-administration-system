0052c23befb2bdda85698871898ce85106e3729f
Listopia-Official <florhas@t-online.de>
2017-01-01 14:44:29 +0100

    Added ValidItemPrice validation constraint

---
+++ lucas-ejb/ejbModule/florian_haas/lucas/model/validation/ValidItemPrice.java
@@ -0,0 +1,24 @@
+package florian_haas.lucas.model.validation;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.*;
+
+import javax.validation.*;
+import javax.validation.constraints.*;
+
+@Target({
+		FIELD, METHOD, PARAMETER, ANNOTATION_TYPE, CONSTRUCTOR })
+@Retention(RUNTIME)
+@Constraint(validatedBy = {})
+@Documented
+@NotNull
+@DecimalMin(value = "0.0", inclusive = false)
+public @interface ValidItemPrice {
+	String message() default "Invalid item price";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+}
