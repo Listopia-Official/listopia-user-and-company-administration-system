63f0599d56364778d105241ad21f657540554d61
Listopia-Official <florhas@t-online.de>
2017-01-09 15:49:17 +0100

    Added VisaDAO

---
+++ lucas-ejb/ejbModule/florian_haas/lucas/database/VisaDAO.java
@@ -0,0 +1,5 @@
+package florian_haas.lucas.database;
+
+import florian_haas.lucas.model.Visa;
+
+public interface VisaDAO extends ReadOnlyDAO<Visa> {}
---
+++ lucas-ejb/ejbModule/florian_haas/lucas/database/impl/VisaDAOImpl.java
@@ -0,0 +1,7 @@
+package florian_haas.lucas.database.impl;
+
+import florian_haas.lucas.database.*;
+import florian_haas.lucas.model.Visa;
+
+@JPADAO
+public class VisaDAOImpl extends ReadOnlyDAOImpl<Visa> implements VisaDAO {}
