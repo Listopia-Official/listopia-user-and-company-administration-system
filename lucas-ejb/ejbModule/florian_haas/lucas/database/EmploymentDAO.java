8014094a0e07a8f9cf1ba39ececc81b5efd7527c
Listopia-Official <florhas@t-online.de>
2017-01-09 13:45:19 +0100

    Added EmploymentDAO

---
+++ lucas-ejb/ejbModule/florian_haas/lucas/database/EmploymentDAO.java
@@ -0,0 +1,5 @@
+package florian_haas.lucas.database;
+
+import florian_haas.lucas.model.Employment;
+
+public interface EmploymentDAO extends ReadOnlyDAO<Employment> {}
---
+++ lucas-ejb/ejbModule/florian_haas/lucas/database/impl/EmploymentDAOImpl.java
@@ -0,0 +1,7 @@
+package florian_haas.lucas.database.impl;
+
+import florian_haas.lucas.database.*;
+import florian_haas.lucas.model.Employment;
+
+@JPADAO
+public class EmploymentDAOImpl extends ReadOnlyDAOImpl<Employment> implements EmploymentDAO {}
