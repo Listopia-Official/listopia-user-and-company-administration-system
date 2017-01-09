d249978f916489b2dc759c93d6f2a0025cb8e22f
Listopia-Official <florhas@t-online.de>
2017-01-09 14:03:05 +0100

    Added SalaryAttendancedataDAO

---
+++ lucas-ejb/ejbModule/florian_haas/lucas/database/SalaryAttendancedataDAO.java
@@ -0,0 +1,5 @@
+package florian_haas.lucas.database;
+
+import florian_haas.lucas.model.SalaryAttendancedata;
+
+public interface SalaryAttendancedataDAO extends ReadOnlyDAO<SalaryAttendancedata> {}
