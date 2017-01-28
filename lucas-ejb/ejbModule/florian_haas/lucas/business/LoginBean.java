9c0ad90f7cbdd8743075c97eb4b16e02fd4dfbe4
Listopia-Official <florhas@t-online.de>
2017-01-28 18:36:18 +0100

    Removed whitespaces

--- lucas-ejb/ejbModule/florian_haas/lucas/business/LoginBean.java
+++ lucas-ejb/ejbModule/florian_haas/lucas/business/LoginBean.java
@@ -1,14 +1,14 @@
 package florian_haas.lucas.business;
 
 import javax.ejb.*;
-import javax.inject.Inject;
 import javax.validation.executable.*;
 
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.apache.shiro.subject.Subject;
 
-import florian_haas.lucas.util.*;
+import florian_haas.lucas.util.Secured;
 
 @Stateless
 @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
@@ -16,24 +16,21 @@ import florian_haas.lucas.util.*;
 @Secured
 public class LoginBean implements LoginBeanLocal {
 
-	@Inject
-	private SecurityProducer producer;
-
 	@Override
 	public void login(Long userId, char[] password, Boolean rememberMe) {
-		producer.getSubject().login(new UsernamePasswordToken(userId.toString(), password, rememberMe));
+		SecurityUtils.getSubject().login(new UsernamePasswordToken(userId.toString(), password, rememberMe));
 	}
 
 	@Override
 	public void logout() {
-		producer.getSubject().logout();
+		SecurityUtils.getSubject().logout();
 	}
 
 	@Override
 	@RequiresPermissions({
 			"login:getSubject" })
 	public Subject getSubject() {
-		return producer.getSubject();
+		return SecurityUtils.getSubject();
 	}
 
 }
