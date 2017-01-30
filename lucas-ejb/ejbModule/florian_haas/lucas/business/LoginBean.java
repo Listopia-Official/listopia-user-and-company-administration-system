11b4e932f92c76e466ebd5fdf0c1f10e375252d3
Listopia-Official <florhas@t-online.de>
2017-01-30 17:52:19 +0100

    Moved shiro.ini to web project

--- lucas-ejb/ejbModule/florian_haas/lucas/business/LoginBean.java
+++ lucas-ejb/ejbModule/florian_haas/lucas/business/LoginBean.java
@@ -1,14 +1,20 @@
 package florian_haas.lucas.business;
 
+import java.util.Arrays;
+
 import javax.ejb.*;
+import javax.inject.Inject;
 import javax.validation.executable.*;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authc.credential.*;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.apache.shiro.subject.Subject;
 
-import florian_haas.lucas.util.Secured;
+import florian_haas.lucas.database.*;
+import florian_haas.lucas.model.*;
+import florian_haas.lucas.security.Secured;
 
 @Stateless
 @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
@@ -16,9 +22,17 @@ import florian_haas.lucas.util.Secured;
 @Secured
 public class LoginBean implements LoginBeanLocal {
 
+	@Inject
+	@JPADAO
+	private LoginUserDAO loginUserDao;
+
+	@EJB
+	private UserBeanLocal userBean;
+
 	@Override
-	public void login(Long userId, char[] password, Boolean rememberMe) {
-		SecurityUtils.getSubject().login(new UsernamePasswordToken(userId.toString(), password, rememberMe));
+	public void login(String username, char[] password) {
+		SecurityUtils.getSubject().login(new UsernamePasswordToken(username, password, false));
+		Arrays.fill(password, 'c');
 	}
 
 	@Override
@@ -31,6 +45,65 @@ public class LoginBean implements LoginBeanLocal {
 			"login:getSubject" })
 	public Subject getSubject() {
 		return SecurityUtils.getSubject();
+	}
+
+	@Override
+	@RequiresPermissions({
+			"login:createRawLoginUser" })
+	public Long newLoginUser(String username, char[] password) {
+		return newLoginUserHelper(username, password, null);
+	}
+
+	@Override
+	@RequiresPermissions({
+			"login:createRegisteredLoginUser" })
+	public Long newLoginUser(Long user, char[] password) {
+		return newLoginUserHelper(Long.toString(user), password, userBean.findById(user));
+	}
+
+	private Long newLoginUserHelper(String username, char[] password, User usr) {
+		PasswordService passwordService = new DefaultPasswordService();
+		String encryptedPassword = passwordService.encryptPassword(password);
+		LoginUser user = new LoginUser(username, encryptedPassword, usr);
+		Arrays.fill(password, 'c');
+		loginUserDao.persist(user);
+		return user.getId();
+	}
+
+	@Override
+	@RequiresPermissions({
+			"login:changePassword" })
+	public Boolean changePassword(Long loginUserId, char[] oldPassword, char[] newPassword) {
+		return changePasswordHelper(loginUserId, oldPassword, newPassword);
+	}
+
+	@Override
+	@RequiresPermissions({
+			"login:newPassword" })
+	public Boolean newPassword(Long loginUserId, char[] newPassword) {
+		return changePasswordHelper(loginUserId, null, newPassword);
+	}
+
+	private Boolean changePasswordHelper(Long loginUserId, char[] oldPassword, char[] newPassword) {
+		// PasswordService passwordService = new DefaultPasswordService();
+		// LoginUser user = loginUserDao.findById(loginUserId);
+		// String oldHashedPassword = null;
+		// if (oldPassword != null) {
+		// oldHashedPassword = passwordService.enc
+		// Arrays.fill(oldPassword, 'c');
+		// }
+		// String salt = randomSalt();
+		// String newHashedPassword = new Sha512Hash(newPassword, salt).toHex();
+		// Arrays.fill(newPassword, 'c');
+		//
+		// if (!user.getHashedPassword().equals(newHashedPassword)) {
+		// if (oldPassword == null || (oldPassword != null &&
+		// user.getHashedPassword().equals(oldHashedPassword))) {
+		// user.setHashedPassword(newHashedPassword, salt);
+		// return Boolean.TRUE;
+		// }
+		// }
+		return Boolean.FALSE;
 	}
 
 }
--- lucas-ejb/ejbModule/florian_haas/lucas/business/LoginBeanLocal.java
+++ lucas-ejb/ejbModule/florian_haas/lucas/business/LoginBeanLocal.java
@@ -5,15 +5,25 @@ import javax.validation.constraints.NotNull;
 
 import org.apache.shiro.subject.Subject;
 
-import florian_haas.lucas.model.User;
-import florian_haas.lucas.model.validation.ValidEntityId;
+import florian_haas.lucas.model.*;
+import florian_haas.lucas.model.validation.*;
+import florian_haas.lucas.util.validation.NotBlankString;
 
 @Local
 public interface LoginBeanLocal {
 
-	public void login(@ValidEntityId(entityClass = User.class) Long userId, char[] password, @NotNull Boolean rememberMe);
+	public Long newLoginUser(@NotNull @NotBlankString String username, @ValidUnhashedPassword char[] password);
+
+	public Long newLoginUser(@ValidEntityId(entityClass = User.class) Long user, @ValidUnhashedPassword char[] password);
+
+	public void login(@NotNull @NotBlankString String username, @ValidUnhashedPassword char[] password);
 
 	public void logout();
+
+	public Boolean changePassword(@ValidEntityId(entityClass = LoginUser.class) Long loginUserId, @ValidUnhashedPassword char[] oldPassword,
+			@ValidUnhashedPassword char[] newPassword);
+
+	public Boolean newPassword(@ValidEntityId(entityClass = LoginUser.class) Long loginUserId, @ValidUnhashedPassword char[] newPassword);
 
 	public Subject getSubject();
 
