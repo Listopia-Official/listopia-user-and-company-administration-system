c0158694c5341e35f9dd8938f3e1ec30904dd2b4
Listopia-Official <florhas@t-online.de>
2017-02-04 17:50:34 +0100

    Improved Tests

--- lucas-web/src/florian_haas/lucas/web/TestBean.java
+++ lucas-web/src/florian_haas/lucas/web/TestBean.java
@@ -1,12 +1,18 @@
 package florian_haas.lucas.web;
 
 import java.io.Serializable;
+import java.util.*;
 
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import javax.validation.*;
 
 import florian_haas.lucas.business.*;
+import florian_haas.lucas.model.LoginUserRole;
+import florian_haas.lucas.model.validation.ValidUnhashedPassword;
 
 @Named
 @ViewScoped
@@ -14,7 +20,8 @@ public class TestBean implements Serializable {
 
 	private static final long serialVersionUID = -8272863330457069304L;
 
-	private int count = 0;
+	@ValidUnhashedPassword
+	private char[] password = null;
 
 	@EJB
 	private UserBeanLocal userBean;
@@ -22,30 +29,38 @@ public class TestBean implements Serializable {
 	@EJB
 	private LoginBeanLocal loginBean;
 
+	@EJB
+	private EntityBeanLocal entityBean;
+
 	public void newUser() {
-		// for (int i = 0; i < 800; i++) {
-		// userBean.createPupil(Double.toString(Math.random()), Double.toString(Math.random()),
-		// (int) Math.random() + 1,
-		// Double.toString(Math.random()), null);
-		// }
-		// for (int i = 0; i < 100; i++) {
-		// userBean.createTeacher(Double.toString(Math.random()), Double.toString(Math.random()),
-		// null);
-		//
-		// }
-		// for (int i = 0; i < 250; i++) {
-		// userBean.createGuest();
-		// }
+		try {
+			Long id = userBean.createGuest();
+			Long userId = loginBean.newLoginUser(id, password);
+			List<LoginUserRole> roles = loginBean.findAllLoginUserRoles();
+			if (roles.isEmpty()) {
+				Set<String> permissions = new HashSet<>();
+				permissions.add("*");
+				LoginUserRole role = loginBean.findLoginUserRoleById(loginBean.newLoginUserRole("Testrole", permissions));
+				roles.add(role);
+			}
+			loginBean.addLoginUserRole(userId, roles.get(0).getId());
+		}
+		catch (ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			violations.forEach(violation -> {
+				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error:", violation.getMessage()));
+			});
+		}
+		catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error:", e.getMessage()));
+		}
 	}
 
-	public String logout() {
-		loginBean.logout();
-		return "login.xhtml";
+	public void setPassword(char[] password) {
+		this.password = password;
 	}
 
-	public int getCount() {
-		count = userBean.findAll().size();
-		return count;
+	public char[] getPassword() {
+		return this.password;
 	}
-
 }
