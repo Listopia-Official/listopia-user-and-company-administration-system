a4c4df246b8580892d4849a1d3e8328f5a5abf16
Listopia-Official <florhas@t-online.de>
2017-02-04 15:57:14 +0100

    Added findByName() to LoginUserRoleDAO

--- lucas-ejb/ejbModule/florian_haas/lucas/database/LoginUserRoleDAO.java
+++ lucas-ejb/ejbModule/florian_haas/lucas/database/LoginUserRoleDAO.java
@@ -2,4 +2,8 @@ package florian_haas.lucas.database;
 
 import florian_haas.lucas.model.LoginUserRole;
 
-public interface LoginUserRoleDAO extends DAO<LoginUserRole> {}
+public interface LoginUserRoleDAO extends DAO<LoginUserRole> {
+
+	public LoginUserRole findByName(String name);
+
+}
--- lucas-ejb/ejbModule/florian_haas/lucas/database/impl/LoginUserRoleDAOImpl.java
+++ lucas-ejb/ejbModule/florian_haas/lucas/database/impl/LoginUserRoleDAOImpl.java
@@ -1,7 +1,22 @@
 package florian_haas.lucas.database.impl;
 
+import java.util.*;
+
+import javax.persistence.criteria.Predicate;
+
 import florian_haas.lucas.database.*;
-import florian_haas.lucas.model.LoginUserRole;
+import florian_haas.lucas.model.*;
 
 @JPADAO
-public class LoginUserRoleDAOImpl extends DAOImpl<LoginUserRole> implements LoginUserRoleDAO {}
+public class LoginUserRoleDAOImpl extends DAOImpl<LoginUserRole> implements LoginUserRoleDAO {
+
+	@Override
+	public LoginUserRole findByName(String name) {
+		List<LoginUserRole> results = readOnlyCriteriaQuery((query, root, builder) -> {
+			List<Predicate> predicates = new ArrayList<>();
+			getSingularRestriction(LoginUserRole_.name, name, Boolean.TRUE, null, predicates, builder, root);
+			return predicates.toArray(new Predicate[predicates.size()]);
+		});
+		return results.size() > 0 ? results.get(0) : null;
+	}
+}
