14717623e2073e9585155b801e38902e346dee97
Listopia-Official <florhas@t-online.de>
2017-02-12 20:58:39 +0100

    Added WebUtils

---
+++ lucas-web/src/florian_haas/lucas/util/WebUtils.java
@@ -0,0 +1,68 @@
+package florian_haas.lucas.util;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+public class WebUtils {
+
+	public static void addInformationMessage(String message) {
+		addMessage(FacesMessage.SEVERITY_INFO, message);
+	}
+
+	public static void addInformationMessage(String message, Object... params) {
+		addMessage(FacesMessage.SEVERITY_INFO, message, params);
+	}
+
+	public static void addWarningMessage(String message) {
+		addMessage(FacesMessage.SEVERITY_WARN, message);
+	}
+
+	public static void addWarningMessage(String message, Object... params) {
+		addMessage(FacesMessage.SEVERITY_WARN, message, params);
+	}
+
+	public static void addErrorMessage(String message) {
+		addMessage(FacesMessage.SEVERITY_ERROR, message);
+	}
+
+	public static void addErrorMessage(String message, Object... params) {
+		addMessage(FacesMessage.SEVERITY_ERROR, message, params);
+	}
+
+	public static void addFatalMessage(String message) {
+		addMessage(FacesMessage.SEVERITY_FATAL, message);
+	}
+
+	public static void addFatalMessage(String message, Object... params) {
+		addMessage(FacesMessage.SEVERITY_FATAL, message, params);
+	}
+
+	private static void addMessage(FacesMessage.Severity severity, String key, Object... params) {
+		addMessage(severity, getTranslatedMessage(key, params));
+	}
+
+	private static void addMessage(FacesMessage.Severity severity, String message) {
+		String titlePrefix = severity == FacesMessage.SEVERITY_WARN ? "warn"
+				: severity == FacesMessage.SEVERITY_ERROR ? "error" : severity == FacesMessage.SEVERITY_FATAL ? "fatal" : "info";
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(severity, getTranslatedMessage("lucas.application.message." + titlePrefix), message));
+	}
+
+	public static String getTranslatedMessage(String key, Object... params) {
+		String message = getTranslatedMessage(key);
+		for (int i = 0; i < params.length; i++) {
+			message = message.replaceAll("\\?" + i, params[i].toString());
+		}
+		return message;
+	}
+
+	public static String getTranslatedMessage(String key) {
+		return FacesContext.getCurrentInstance().getApplication().evaluateExpressionGet(FacesContext.getCurrentInstance(), "#{msg['" + key + "']}",
+				String.class);
+	}
+
+	public static void executeTask() {
+
+	}
+
+}
