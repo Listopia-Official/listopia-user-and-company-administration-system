53c273d568c364d55b936af4f081392f140d6f93
Listopia-Official <florhas@t-online.de>
2017-02-17 16:39:33 +0100

    Categories are now created once

--- lucas-ejb/ejbModule/florian_haas/lucas/database/EnumQueryComparator.java
+++ lucas-ejb/ejbModule/florian_haas/lucas/database/EnumQueryComparator.java
@@ -3,23 +3,31 @@ package florian_haas.lucas.database;
 public enum EnumQueryComparator {
 	EQUAL, NOT_EQUAL, GREATER_THAN, LESS_THAN, GREATER_EQUAL, LESS_EQUAL, MEMBER_OF, NOT_MEMBER_OF, LIKE, NOT_LIKE;
 
+	private static final EnumQueryComparator[] numericComparators = new EnumQueryComparator[] {
+			EQUAL, NOT_EQUAL, GREATER_THAN, LESS_THAN, GREATER_EQUAL, LESS_EQUAL };
+
+	private static final EnumQueryComparator[] arrayComparators = new EnumQueryComparator[] {
+			MEMBER_OF, NOT_MEMBER_OF };
+
+	private static final EnumQueryComparator[] textComparators = new EnumQueryComparator[] {
+			EQUAL, NOT_EQUAL, GREATER_THAN, LESS_THAN, GREATER_EQUAL, LESS_EQUAL, LIKE, NOT_LIKE };
+
+	private static final EnumQueryComparator[] logicComparators = new EnumQueryComparator[] {
+			EQUAL, NOT_EQUAL };
+
 	public static EnumQueryComparator[] getNumericComparators() {
-		return new EnumQueryComparator[] {
-				EQUAL, NOT_EQUAL, GREATER_THAN, LESS_THAN, GREATER_EQUAL, LESS_EQUAL };
+		return numericComparators;
 	}
 
 	public static EnumQueryComparator[] getArrayComparators() {
-		return new EnumQueryComparator[] {
-				MEMBER_OF, NOT_MEMBER_OF };
+		return arrayComparators;
 	}
 
 	public static EnumQueryComparator[] getTextComparators() {
-		return new EnumQueryComparator[] {
-				EQUAL, NOT_EQUAL, GREATER_THAN, LESS_THAN, GREATER_EQUAL, LESS_EQUAL, LIKE, NOT_LIKE };
+		return textComparators;
 	}
 
 	public static EnumQueryComparator[] getLogicComparators() {
-		return new EnumQueryComparator[] {
-				EQUAL, NOT_EQUAL };
+		return logicComparators;
 	}
 }
