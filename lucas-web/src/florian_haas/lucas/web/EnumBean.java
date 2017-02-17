e420512bc9033b2cf984f33d722a375f1210f7f3
Listopia-Official <florhas@t-online.de>
2017-02-17 15:38:46 +0100

    Added EnumBean

---
+++ lucas-web/src/florian_haas/lucas/web/EnumBean.java
@@ -0,0 +1,33 @@
+package florian_haas.lucas.web;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+import florian_haas.lucas.database.EnumQueryComparator;
+import florian_haas.lucas.model.EnumSchoolClass;
+
+@Named
+@ApplicationScoped
+public class EnumBean implements Serializable {
+
+	private static final long serialVersionUID = -3382424211648556728L;
+
+	public EnumSchoolClass[] getEnumSchoolClassValues() {
+		return EnumSchoolClass.values();
+	}
+
+	public EnumQueryComparator[] getTextComparators() {
+		return EnumQueryComparator.getTextComparators();
+	}
+
+	public EnumQueryComparator[] getNumericComparators() {
+		return EnumQueryComparator.getNumericComparators();
+	}
+
+	public EnumQueryComparator[] getArrayComparators() {
+		return EnumQueryComparator.getArrayComparators();
+	}
+
+}
