4ef2e7947a4667002f06329fb6de6251d9e53810
Listopia-Official <florhas@t-online.de>
2017-02-20 14:37:51 +0100

    Added OutputTextRenderer

---
+++ lucas-web/src/florian_haas/lucas/util/renderer/OutputTextRenderer.java
@@ -0,0 +1,19 @@
+package florian_haas.lucas.util.renderer;
+
+import javax.faces.component.*;
+import javax.faces.context.FacesContext;
+
+import com.sun.faces.renderkit.html_basic.TextRenderer;
+
+public class OutputTextRenderer extends TextRenderer {
+
+	@Override
+	protected String getCurrentValue(FacesContext context, UIComponent component) {
+		if ((component instanceof UIInput)) {
+			Object submittedValue = ((UIInput) component).getSubmittedValue();
+			if (submittedValue != null) return submittedValue.toString();
+		}
+		return getFormattedValue(context, component, getValue(component));
+	}
+
+}
