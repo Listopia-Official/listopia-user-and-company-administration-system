67eeb0e9102d4bbeac61bba6f06632b9d01c1a04
Listopia-Official <florhas@t-online.de>
2017-02-24 19:21:15 +0100

    Added ConverterBean

---
+++ lucas-web/src/florian_haas/lucas/web/ConverterBean.java
@@ -0,0 +1,31 @@
+package florian_haas.lucas.web;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+import florian_haas.lucas.util.WebUtils;
+
+@Named
+@ApplicationScoped
+public class ConverterBean implements Serializable {
+
+	private static final long serialVersionUID = 5695265421704720573L;
+
+	public static final String NULL_KEY = "lucas.application.converterBean.nullValue";
+
+	public <E extends Enum<E>> String convertEnum(E enumValue) {
+		String ret;
+		if (enumValue != null) {
+			FacesContext context = FacesContext.getCurrentInstance();
+			ret = context.getApplication().createConverter(enumValue.getClass()).getAsString(context, UIComponent.getCurrentComponent(context),
+					enumValue);
+		} else {
+			ret = WebUtils.getTranslatedMessage(NULL_KEY);
+		}
+		return ret;
+	}
+}
