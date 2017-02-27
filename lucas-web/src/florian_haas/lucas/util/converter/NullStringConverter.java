d2df7a0490b3479ac36bc18e40e9e44a51851379
Listopia-Official <florhas@t-online.de>
2017-02-27 14:41:57 +0100

    Fixed spelling error in lang key

--- lucas-web/src/florian_haas/lucas/util/converter/NullStringConverter.java
+++ lucas-web/src/florian_haas/lucas/util/converter/NullStringConverter.java
@@ -9,7 +9,7 @@ import florian_haas.lucas.util.WebUtils;
 @FacesConverter(value = "lucas:NullStringConverter")
 public class NullStringConverter implements Converter {
 
-	public static final String NO_VALUE_KEY = "lucas.application.emtryStringConverter.empty";
+	public static final String NO_VALUE_KEY = "lucas.application.emptyStringConverter.empty";
 
 	@Override
 	public Object getAsObject(FacesContext context, UIComponent component, String value) {
