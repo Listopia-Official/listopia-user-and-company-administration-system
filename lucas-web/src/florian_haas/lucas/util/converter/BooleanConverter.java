34cff6701a4f4ff1e06684bf494af13dd77baa26
Listopia-Official <florhas@t-online.de>
2017-03-03 10:45:43 +0100

    Closed #27

---
+++ lucas-web/src/florian_haas/lucas/util/converter/BooleanConverter.java
@@ -0,0 +1,29 @@
+package florian_haas.lucas.util.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.*;
+
+import florian_haas.lucas.util.WebUtils;
+
+@FacesConverter(value = "lucas:BooleanStringConverter")
+public class BooleanConverter implements Converter {
+
+	public static final String NULL_KEY = "lucas.application.booleanConverter.null";
+	public static final String TRUE_KEY = "lucas.application.booleanConverter.true";
+	public static final String FALSE_KEY = "lucas.application.booleanConverter.false";
+
+	@Override
+	public Object getAsObject(FacesContext arg0, UIComponent arg1, String arg2) {
+		return arg2 == null || arg2.equals(WebUtils.getTranslatedMessage(NULL_KEY)) ? null
+				: arg2.equals(WebUtils.getTranslatedMessage(TRUE_KEY)) ? Boolean.TRUE : Boolean.FALSE;
+	}
+
+	@Override
+	public String getAsString(FacesContext arg0, UIComponent arg1, Object arg2) {
+		Boolean bool = (Boolean) arg2;
+		String key = bool == null ? NULL_KEY : bool == Boolean.TRUE ? TRUE_KEY : FALSE_KEY;
+		return WebUtils.getTranslatedMessage(key);
+	}
+
+}
