a3800d482095cf071f0d13265aca3101a50618eb
Listopia-Official <florhas@t-online.de>
2017-03-18 19:35:03 +0100

    Fixed query returning no results if user or roles is NULL or EMPTY

--- lucas-ejb/ejbModule/florian_haas/lucas/database/impl/LoginUserDAOImpl.java
+++ lucas-ejb/ejbModule/florian_haas/lucas/database/impl/LoginUserDAOImpl.java
@@ -2,7 +2,7 @@ package florian_haas.lucas.database.impl;
 
 import java.util.*;
 
-import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.*;
 
 import florian_haas.lucas.database.*;
 import florian_haas.lucas.model.*;
@@ -14,15 +14,17 @@ public class LoginUserDAOImpl extends DAOImpl<LoginUser> implements LoginUserDAO
 	public List<LoginUser> findLoginUsers(Long id, String username, Long userId, List<Long> roleIds, Boolean useId, Boolean useUsername,
 			Boolean useUserId, Boolean useRoleIds, EnumQueryComparator idComparator, EnumQueryComparator usernameComparator,
 			EnumQueryComparator userIdComparator, EnumQueryComparator roleIdsComparator) {
-		return this.readOnlyCriteriaQuery((query, root, builder) -> {
+		List<LoginUser> ret = this.readOnlyCriteriaQuery((query, root, builder) -> {
 			List<Predicate> predicates = new ArrayList<>();
 
 			getSingularRestriction(LoginUser_.id, id, useId, idComparator, predicates, builder, root);
 			getSingularRestriction(LoginUser_.username, username, useUsername, usernameComparator, predicates, builder, root);
-			getSingularRestriction(root.join(LoginUser_.user).get(User_.id), userId, useUserId, userIdComparator, predicates, builder, root);
-			getSingularRestrictionCollection(root.join(LoginUser_.roles).get(LoginUserRole_.id), roleIds, useRoleIds, roleIdsComparator, predicates,
-					builder, root);
+			getSingularRestriction(root.join(LoginUser_.user, JoinType.LEFT).get(User_.id), userId, useUserId, userIdComparator, predicates, builder,
+					root);
+			getSingularRestrictionCollection(root.join(LoginUser_.roles, JoinType.LEFT).get(LoginUserRole_.id), roleIds, useRoleIds,
+					roleIdsComparator, predicates, builder, root);
 			return predicates.toArray(new Predicate[predicates.size()]);
 		});
+		return ret;
 	}
 }
