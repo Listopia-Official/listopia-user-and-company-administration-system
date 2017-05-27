351c9938ae15498c67bc401ba1a6b364280bd85d
Listopia-Official <florhas@t-online.de>
2017-05-27 15:48:36 +0200

    Closed #128

--- lucas-ejb/ejbModule/florian_haas/lucas/validation/ValidCompanyHierarchyValidator.java
+++ lucas-ejb/ejbModule/florian_haas/lucas/validation/ValidCompanyHierarchyValidator.java
@@ -2,7 +2,7 @@ package florian_haas.lucas.validation;
 
 import javax.validation.*;
 
-import florian_haas.lucas.model.*;
+import florian_haas.lucas.model.Company;
 
 public class ValidCompanyHierarchyValidator implements ConstraintValidator<ValidCompanyHierarchy, Company> {
 
@@ -11,10 +11,14 @@ public class ValidCompanyHierarchyValidator implements ConstraintValidator<Valid
 
 	@Override
 	public boolean isValid(Company value, ConstraintValidatorContext context) {
-		if (value == null) return true;
-		if (value.getParentCompany() != null && value.getParentCompany().equals(value)) return false;
-		for (ReadOnlyCompany child : value.getChildCompanies()) {
-			if (child.equals(value)) return false;
+		return (value.getParentCompany() != null ? !value.getParentCompany().equals(value) : true) && isValid(value, (Company) null);
+	}
+
+	private boolean isValid(Company start, Company root) {
+		if (start == null) return true;
+		if (start.equals(root)) return false;
+		for (Company child : start.getChildCompanies()) {
+			if (!isValid(child, root == null ? start : root)) return false;
 		}
 		return true;
 	}
