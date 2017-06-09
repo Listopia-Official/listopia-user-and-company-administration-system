d663514892f75b7f78888c4087b2d13b6c705c0f
Listopia-Official <florhas@t-online.de>
2017-06-09 16:38:09 +0200

    Added missing groups

--- lucas-ejbClient/ejbModule/florian_haas/lucas/persistence/EnumQueryComparator.java
+++ lucas-ejbClient/ejbModule/florian_haas/lucas/persistence/EnumQueryComparator.java
@@ -6,11 +6,17 @@ public enum EnumQueryComparator {
 	public static final EnumQueryComparator[] NUMERIC_COMPARATORS = new EnumQueryComparator[] {
 			EQUAL, NOT_EQUAL, GREATER_THAN, LESS_THAN, GREATER_EQUAL, LESS_EQUAL, NULL, NOT_NULL };
 
+	public static final EnumQueryComparator[] NUMERIC_COMPARATORS_NOT_NULL = new EnumQueryComparator[] {
+			EQUAL, NOT_EQUAL, GREATER_THAN, LESS_THAN, GREATER_EQUAL, LESS_EQUAL };
+
 	public static final EnumQueryComparator[] ARRAY_COMPARATORS = new EnumQueryComparator[] {
 			MEMBER_OF, NOT_MEMBER_OF, EMPTY, NOT_EMPTY };
 
 	public static final EnumQueryComparator[] TEXT_COMPARATORS = new EnumQueryComparator[] {
 			EQUAL, NOT_EQUAL, GREATER_THAN, LESS_THAN, GREATER_EQUAL, LESS_EQUAL, LIKE, NOT_LIKE, NOT_NULL, NULL };
+
+	public static final EnumQueryComparator[] TEXT_COMPARATORS_NOT_NULL = new EnumQueryComparator[] {
+			EQUAL, NOT_EQUAL, GREATER_THAN, LESS_THAN, GREATER_EQUAL, LESS_EQUAL, LIKE, NOT_LIKE };
 
 	public static final EnumQueryComparator[] LOGIC_COMPARATORS = new EnumQueryComparator[] {
 			EQUAL, NOT_EQUAL, NOT_NULL, NULL };
