2de70560918a79e38c80a951821ce89f85b4c2eb
Listopia-Official <florhas@t-online.de>
2017-07-16 16:43:48 +0200

    Added support for getAsObject

--- lucas-web/src/florian_haas/lucas/web/converter/LongToTimeConverter.java
+++ lucas-web/src/florian_haas/lucas/web/converter/LongToTimeConverter.java
@@ -1,5 +1,6 @@
 package florian_haas.lucas.web.converter;
 
+import java.util.StringTokenizer;
 import java.util.concurrent.TimeUnit;
 
 import javax.faces.component.UIComponent;
@@ -13,18 +14,32 @@ public class LongToTimeConverter extends BasicConverter<Long> {
 
 	public static final String CONVERTER_ID = "lucas:longToTimeConverter";
 
+	public LongToTimeConverter() {
+		this.setIsReadonly(Boolean.FALSE);
+	}
+
 	@Override
 	protected String getString(FacesContext context, UIComponent uiComponent, Long value) {
 		// long mls = value % 1000;
+		String separator = WebUtils.getTranslatedMessage("lucas.application.longToTimeConverter.separator");
 		return value != null
-				? WebUtils.getTranslatedMessage("lucas.application.longToTimeConverter.normal",
-						formattLong(TimeUnit.MILLISECONDS.toHours(value) % 24), formattLong(TimeUnit.MILLISECONDS.toMinutes(value) % 60),
-						formattLong(TimeUnit.MILLISECONDS.toSeconds(value) % 60))
-				: WebUtils.getTranslatedMessage("lucas.application.longToTimeConverter.normal");
+				? formattLong(TimeUnit.MILLISECONDS.toHours(value) % 24) + separator + formattLong(TimeUnit.MILLISECONDS.toMinutes(value) % 60)
+						+ separator + formattLong(TimeUnit.MILLISECONDS.toSeconds(value) % 60)
+				: WebUtils.getTranslatedMessage("lucas.application.longToTimeConverter.null");
 	}
 
 	private String formattLong(Long data) {
 		return String.format("%02d", data);
+	}
+
+	@Override
+	protected Long getObject(FacesContext context, UIComponent component, String value) {
+		if (value.trim().equals(WebUtils.getTranslatedMessage("lucas.application.longToTimeConverter.null"))) return null;
+		StringTokenizer tok = new StringTokenizer(value, WebUtils.getTranslatedMessage("lucas.application.longToTimeConverter.separator"));
+		Long hours = Long.parseLong(tok.nextToken());
+		Long minutes = Long.parseLong(tok.nextToken());
+		Long seconds = Long.parseLong(tok.nextToken());
+		return TimeUnit.HOURS.toMillis(hours) + TimeUnit.MINUTES.toMillis(minutes) + TimeUnit.SECONDS.toMillis(seconds);
 	}
 
 }
